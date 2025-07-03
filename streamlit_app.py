dc6d8f2dee05d00fe004c46edbd7fd28829bb1bc
chev57 <kdevroy@sbcglobal.net>
2025-07-03 09:48:59 -0500

    Update streamlit_app.py
    fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.write(
   """Choose the fruits you want in your custom Smoothie!.
   """)
 
-name_on_order = st.text_input('Name on Smmoothie:')
+name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
