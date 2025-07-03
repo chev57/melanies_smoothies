80e7c4a07c0629d851bca51948335591e3f9efd6
chev57 <kdevroy@sbcglobal.net>
2025-07-03 08:31:36 -0500

    Update streamlit_app.py
    no nutrition

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,8 +42,8 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered, ' +name_on_order + '!', icon="✅")
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+#import requests
+#smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
-st.write("new version")
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+#st.write("new version")
+#sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
